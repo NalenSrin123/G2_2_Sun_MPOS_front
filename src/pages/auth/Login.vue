5a6568a0d15fa7745fd04d34b965916e460b687f
unknown <ngunmakara009@gmail.com>
2026-06-07 16:20:37 +0700

    fix-icon-eye-and-click-to-login

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -96,7 +96,7 @@
               <span class="text-gray-700">Remember me</span>
             </div>
 
-            <a href="#" class="text-[#216946] font-semibold hover:underline">
+            <a href="" class="text-[#216946] font-semibold hover:underline">
               Forgot password?
             </a>
           </div>
