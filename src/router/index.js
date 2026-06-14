07beaaf64e6b4b8d184d480fd503c8baa347ba98
satogris100-del <satogris100@gmail.com>
2026-06-14 15:38:34 +0700

    fix_inventory list

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,6 @@ import TableForm from "@/pages/dashboard/TableForm.vue";
 import TableList from "@/pages/table/Tablelist.vue";
 import Orders from "@/pages/orders/Orders.vue";
 
-
 import Login from "@/pages/auth/Login.vue";
 import Register from "@/pages/auth/Register.vue";
 import ResetPassword from "@/pages/auth/ResetPassword.vue";
@@ -29,80 +28,70 @@ import QR_Payment from "../../payment/QR_Payment.vue";
 import ProductDetail from "@/pages/dashboard/ProductDetail.vue";
 
 import PaymentMethod from "../pages/orders/PaymentMethod.vue";
+import InventoryList from "@/pages/dashboard/InventoryList.vue";
 
 const routes = [
-  { path: "/", redirect: "/home" },
-  { path: "/preview", component: Preview },
-  { path: "/home", component: Home },
-  { path: "/Home", redirect: "/home" },
- 
+	{ path: "/", redirect: "/home" },
+	{ path: "/preview", component: Preview },
+	{ path: "/home", component: Home },
+	{ path: "/Home", redirect: "/home" },
 
-  {
-    path: "/",
-    component: Sidebar,
-    children: [
-      { path: "dashboard", component: Dashboard },
-      { path: "dashboard/overview", component: DashboardOverview },
-      { path: "orders", component: Orders },
-      { path: "tables", component: TableList },
-      { path: "tables/new", component: TableForm },
-      { path: "products", component: ProductList },
-      { path: "categories", component: CategoryList },
-      { path: "categories/new", component: Category },
-      { path: "settings", component: Setting },
-      { path: "users", component: UserList },
+	{
+		path: "/",
+		component: Sidebar,
+		children: [
+			{ path: "dashboard", component: Dashboard },
+			{ path: "dashboard/overview", component: DashboardOverview },
+			{ path: "orders", component: Orders },
+			{ path: "tables", component: TableList },
+			{ path: "tables/new", component: TableForm },
+			{ path: "products", component: ProductList },
+			{ path: "categories", component: CategoryList },
+			{ path: "categories/new", component: Category },
+			{ path: "settings", component: Setting },
+			{ path: "users", component: UserList },
 
-    
-      { path: "products", component: ProductList },
-      { path: "products/detail/:id", component: ProductDetail },
-      {path: "payment", component: PaymentMethod}
-    ],
-  },
+			{ path: "products", component: ProductList },
+			{ path: "products/detail/:id", component: ProductDetail },
+			{ path: "payment", component: PaymentMethod },
+		],
+	},
 
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/reset-password", component: ResetPassword },
-  { path: "/reset_pas`~word", redirect: "/reset-password" },
-  { path: "/confirm-otp", component: ConfirmOTP },
-  { path: "/forgot-password", component: SendEmail },
-  { path: "/user-form", component: UserForm },
+	{ path: "/login", component: Login },
+	{ path: "/register", component: Register },
+	{ path: "/reset-password", component: ResetPassword },
+	{ path: "/reset_pas`~word", redirect: "/reset-password" },
+	{ path: "/confirm-otp", component: ConfirmOTP },
+	{ path: "/forgot-password", component: SendEmail },
+	{ path: "/user-form", component: UserForm },
 
-  { path: "/:pathMatch(.*)*", component: NotFound },
+	{ path: "/:pathMatch(.*)*", component: NotFound },
 
-    {path:  "/payment", component:QR_Payment},
-{
-  path: "/design_create_product_page",
-  name: "design_create_product_page",
-  component: Dsign_Create_Product_Page,
-}
-
+	{ path: "/payment", component: QR_Payment },
+	{
+		path: "/design_create_product_page",
+		name: "design_create_product_page",
+		component: Dsign_Create_Product_Page,
+	},
 ];
-const route=[
-    {
-      path:"/",
-      children:[
-
-      ]
-        
-    },
-    {
-      path:"/dashboard",
-      component:Sidebar,
-      children:[
-
-      ]
-    },
-    {
-      path: "/auth",
-      children:[
-
-      ]
-    }
-]
+const route = [
+	{
+		path: "/",
+		children: [],
+	},
+	{
+		path: "/dashboard",
+		component: Sidebar,
+		children: [{ path: "inventory", component: InventoryList }],
+	},
+	{
+		path: "/auth",
+		children: [],
+	},
+];
 const router = createRouter({
-  history: createWebHistory(),
-  route,
+	history: createWebHistory(),
+	route,
 });
 
 export default router;
- 
